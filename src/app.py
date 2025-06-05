b7f4838c718b43ba3fffb8a435debcdfc08a14fc
Karen Haining <khaining@cs.washington.edu>
2025-06-05 12:44:46 -0700

    add error handling + hand processing

--- src/app.py
+++ src/app.py
@@ -1,14 +1,10 @@
-import time
 import base64
 import cv2
+
 from flask import Flask, jsonify, make_response, request
 from flask_cors import CORS
-import numpy as np
-from game_state import board
-from game_state import hand
+
 from vision import process_board
-from vision import util
-from vision import config
 from vision import process_hand
 from vision import letter_classifier
 
@@ -18,27 +14,39 @@ def cv_to_msg(img):
     jpg_as_text = base64.b64encode(buffer).decode('utf-8')
     return f'data:image/jpg;base64,{jpg_as_text}'
 
+BP = process_board.BoardProcessor()
+HP = process_hand.HandProcessor()
+CLASSIFIER = letter_classifier.LetterModelClassifier()
+CLASSIFIER.load()
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-@app.route('/cat')
-def get_cat_response():
-    response = make_response(jsonify({'cat': 'meow'}))
-    response.status_code = 200
-    return response
+@app.errorhandler(Exception)
+def handle_exception(e):
+    return jsonify(str(e)), 500
     
 @app.route('/board', methods=['POST'])
 def get_board():
     body = request.get_json()
-    bp = process_board.BoardProcessor()
-    bp.set_image_from_msg(body)
-    bp.crop_to_board()
-    bp.process_tiles()
-    letter_model = letter_classifier.LetterModelClassifier()
-    letter_model.load()
-    letters = letter_model.classify_all(bp)
-    response = make_response(jsonify({'board': letters}))
+
+    try:
+        BP.clear()
+        BP.set_image_from_msg(body)
+        BP.crop_to_board()
+        BP.process_tiles()
+
+        HP.clear()
+        HP.set_image_from_msg(body)
+        HP.crop_to_hand()
+        HP.process_tiles()
+
+        board_letters = CLASSIFIER.classify_all(BP)
+        hand_letters = CLASSIFIER.classify_all(HP)
+    except Exception as e:
+        raise e
+
+    response = make_response(jsonify({'board': board_letters, 'hand': hand_letters}))
     response.status_code = 200
     return response
 
