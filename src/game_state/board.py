b8f611e76560132ea803f9978fb47c18b9127361
Karen Haining <khaining@cs.washington.edu>
2025-06-08 15:14:06 -0700

    actual for realsies fixing this time

--- src/game_state/board.py
+++ src/game_state/board.py
@@ -12,46 +12,46 @@ EMPTY = ''
 # To validate the board state for one-off misidentification
 # Uses a priority queue to keep track of the most-identified letter at that spot
 # Special case handling for empty identifications
-class LetterState:
-    def __init__(self):
-        self.q = []
-        heapq.heappush(self.q, (0, EMPTY))
+# class LetterState:
+#     def __init__(self):
+#         self.q = []
+#         heapq.heappush(self.q, (0, EMPTY))
 
-    def add_letter(self, letter):
-        found = False
-        temp = []
-        for priority, ltr in self.q:
-            if ltr == letter:
-                found = True
-                # -1 because it removes min priority first
-                heapq.heappush(temp, (priority - 1, letter))
-            else:
-                heapq.heappush(temp, (priority, ltr))
-        if not found:
-            heapq.heappush(temp, (-1, letter))
-        self.q = temp
+#     def add_letter(self, letter):
+#         found = False
+#         temp = []
+#         for priority, ltr in self.q:
+#             if ltr == letter:
+#                 found = True
+#                 # -1 because it removes min priority first
+#                 heapq.heappush(temp, (priority - 1, letter))
+#             else:
+#                 heapq.heappush(temp, (priority, ltr))
+#         if not found:
+#             heapq.heappush(temp, (-1, letter))
+#         self.q = temp
 
-    def get_letter(self):
-        # get the first thing from the queue (min value!)
-        # remember this also removes it!
-        priority, letter = heapq.heappop(self.q)
+#     def get_letter(self):
+#         # get the first thing from the queue (min value!)
+#         # remember this also removes it!
+#         priority, letter = heapq.heappop(self.q)
 
-        if letter == EMPTY and self.q:
-            # reset EMPTY priority and go for the next item
-            priority, letter = heapq.heappop(self.q)
-            heapq.heappush(self.q, (priority, letter))
-            heapq.heappush(self.q, (0, EMPTY))
-            return letter
-        heapq.heappush(self.q, (priority, letter))
-        return letter
+#         if letter == EMPTY and self.q:
+#             # reset EMPTY priority and go for the next item
+#             priority, letter = heapq.heappop(self.q)
+#             heapq.heappush(self.q, (priority, letter))
+#             heapq.heappush(self.q, (0, EMPTY))
+#             return letter
+#         heapq.heappush(self.q, (priority, letter))
+#         return letter
 
-    # for debugging purposes
-    def __str__(self):
-        ret = ""
-        for item in sorted(self.q):
-            pr, l = item
-            ret += "(" + str(pr) + ", " + l + ");"
-        return ret
+#     # for debugging purposes
+#     def __str__(self):
+#         ret = ""
+#         for item in sorted(self.q):
+#             pr, l = item
+#             ret += "(" + str(pr) + ", " + l + ");"
+#         return ret
 
 
 class Board():
@@ -84,6 +84,6 @@ class Board():
 
         for row in range(self.SIZE):
             for col in range(self.SIZE):
-                rep[row][col] = self.board[row][col].get_letter()
+                rep[row][col] = self.board[row][col]
 
         return rep
